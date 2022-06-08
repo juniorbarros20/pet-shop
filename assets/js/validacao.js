0ea1fa4235ed4f63e2805d2c53fdac04f777f507
juniorbarros20 <58377610+juniorbarros20@users.noreply.github.com>
2022-06-08 14:00:06 -0300

    Atualizando o projeto

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -27,12 +27,36 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
-    }
+    },
     
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.'
+    },
+
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é válido.'
+    },
+
+    logradouro: {
+        valueMissing: 'O campo de lougradouro não pode estar vazio.',
+    },
+
+    cidade: {
+        valueMissing: 'O campo de cidade não pode estar vazio.',
+    },
+
+   estado: {
+        valueMissing: 'O campo de estado não pode estar vazio.',
+    }
+
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
 function validaDataNascimento(input) {
@@ -47,8 +71,79 @@ function validaDataNascimento(input) {
 }
 
 function mairoQue18(data){
-    const dataAtual = new Date()
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCData())
+    console.log(data)
+    
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
+}
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, ' ')
+    let mensagem = ''
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    let cpfValido = true
+
+    valoresRespetido.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerficador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, ' ')
 }
